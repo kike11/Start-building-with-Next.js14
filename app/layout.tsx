f341e88f89c40ba3005c7794ad36a63be57cbabd
Enrique Rodriguez <kike11.rodriguez@gmail.com>
2024-01-11 11:56:07 -0600

    champert 1 al 5 antes de crear la bd Chapeter 6

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import "@/app/ui/global.css";
+import { monserrat } from "./ui/fonts";
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${monserrat}, antialiased`}>{children}</body>
     </html>
   );
 }
